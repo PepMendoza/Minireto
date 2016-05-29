86f4297424e046d1d41cf696546fdf5319bb2677
Jose Mendoza <josemendoza@mbp-de-jose.gateway.2wire.net>
2016-05-28 19:46:16 -0500

    Initial Commit

---
+++ Minireto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Minireto
+//
+//  Created by Jose Mendoza on 28/05/16.
+//  Copyright © 2016 PepCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MiniretoTests/MiniretoTests.m
@@ -0,0 +1,39 @@
+//
+//  MiniretoTests.m
+//  MiniretoTests
+//
+//  Created by Jose Mendoza on 28/05/16.
+//  Copyright © 2016 PepCo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiniretoTests : XCTestCase
+
+@end
+
+@implementation MiniretoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
